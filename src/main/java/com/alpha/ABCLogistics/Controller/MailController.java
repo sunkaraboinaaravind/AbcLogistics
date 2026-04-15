91e86ed7049210b842a0b548924bb0c3a7e035db
sunkaraboinaaravind <163123710+sunkaraboinaaravind@users.noreply.github.com>
2026-04-15 20:57:46 +0530

    Update recipient email address in sendMail method

--- src/main/java/com/alpha/ABCLogistics/Controller/MailController.java
+++ src/main/java/com/alpha/ABCLogistics/Controller/MailController.java
@@ -12,7 +12,7 @@ public class MailController {
 	MailService mailService;
 	@PostMapping("/sendmail")
 	public void sendMail() {
-		String tomail = "shashankpulluri5@gmail.com";
+		String tomail = "sunkaraboinaaravind123@gmail.com";
 		String sub = "Test Mail";
 		String content = "This is a test mail";
 		mailService.sendMail(tomail, sub, content);
